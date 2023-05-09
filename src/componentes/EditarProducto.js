fcff207894ace3ab92a0d515ffec91cb2dd09e31
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-09 12:36:16 +0200

    Update EditarProducto.js

--- src/componentes/EditarProducto.js
+++ src/componentes/EditarProducto.js
@@ -64,7 +64,7 @@ const EditarProducto = () => {
             id: params.id
         }
 
-        axios.post("https://backend-lobelbuy.vercel.app/mostrarFichaProducto", datos)
+        axios.post("https://backend-lobelbuy.onrender.com/mostrarFichaProducto", datos)
         .then(res => {
             setProducto(res.data);
         })
