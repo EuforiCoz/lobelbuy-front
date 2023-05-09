187b94f13265083b7033a00063472814c1074dd7
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-09 17:10:04 +0200

    Update MiLi.js

--- src/componentes/MiLi.js
+++ src/componentes/MiLi.js
@@ -2,23 +2,26 @@ import React from "react"
 import "./styles/sidebar.css"
 import {BsFillPersonFill} from 'react-icons/bs';
 import {RiMessage2Fill} from 'react-icons/ri';
+import {BsFillInboxFill} from 'react-icons/bs';
+import {RiSettings4Fill} from 'react-icons/ri';
 import {Link} from "react-router-dom"
 
 
-const Sidebar = (props) => {
+const MiLi = (props) => {
 
     //const claseCSS = "nav-link active"
 
     return(
 
-        <li class="nav-item">
+        <li class="nav-item fs-5">
             <Link class={props.clase} to={props.enlace}>
                 <i class="fa fa-home"></i>
                 {props.nombre == "Perfil" && <BsFillPersonFill/>}
+                {props.nombre == "Productos" && <BsFillInboxFill/>}
                 {props.nombre == "Chats" && <RiMessage2Fill/>}
-               
-               
-                {props.nombre}
+                {props.nombre == "Configuración" && <RiSettings4Fill/>}
+                <span className="mx-2">{props.nombre}</span>
+                
             </Link>
         </li>
                        
@@ -26,4 +29,4 @@ const Sidebar = (props) => {
       
 }
 
-export default Sidebar;
+export default MiLi;
