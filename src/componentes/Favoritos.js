5faaec4a38c8ee80bb21d9bf7043ca21995228ae
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-10 10:11:00 +0200

    Update Favoritos.js

--- src/componentes/Favoritos.js
+++ src/componentes/Favoritos.js
@@ -1,4 +1,5 @@
 
+import Sidebar from "./Sidebar";
 
 const Favoritos = () => {
     <div id="favoritos" className=" position-relative" style={{minHeight: "80%", background: "linear-gradient(to bottom, #1E90FF,#87CEEB)"}}>
