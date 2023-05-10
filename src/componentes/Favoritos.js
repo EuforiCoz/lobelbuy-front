78ab1e51a92434f9f983c82254dda098c52f1011
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-10 10:06:43 +0200

    Create Favoritos.js

---
+++ src/componentes/Favoritos.js
@@ -0,0 +1,10 @@
+
+
+const Favoritos = () => {
+    <div id="favoritos" className=" position-relative" style={{minHeight: "80%", background: "linear-gradient(to bottom, #1E90FF,#87CEEB)"}}>
+            
+            <Sidebar componente="favoritos"/>
+    </div>
+}
+
+export default Favoritos;
