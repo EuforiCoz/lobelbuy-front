4674e20af367687406355747dcea5e6f730f47ba
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-16 00:08:12 +0200

    Update Productos.js

--- src/componentes/Productos.js
+++ src/componentes/Productos.js
@@ -5,8 +5,11 @@ import { useAppContext } from "../AppProvider";
 import Sidebar from "./Sidebar";
 import imagenPerfil from "./iconos/carrito.jpg"
 import axios from "axios"
-import { Link,useNavigate } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
+import {Link} from "react-router-dom"
 import imgProducto from "./iconos/crash.jpg"
+import {FaRegHandshake} from "react-icons/fa"
+import imgSale from "./iconos/cupon.png"
 
 const Productos = () => {
 
@@ -15,14 +18,17 @@ const Productos = () => {
     const usuarioConectado = JSON.parse(window.localStorage.getItem('usuario'));
     
     const [productos, setProductos] = useState([]);
+    const [productosEnVenta, setProductosEnVenta] = useState([]);
+    const [productosVendidos, setProductosVendidos] = useState([]);
     const [conversaciones, setConversaciones] = useState([]);
     const [productoSeleccionadoVender, setProductoSeleccionadoVender] = useState();
+    const [mensajeMostrar, setMensajeMostrar] = useState("");
 
     useEffect(()=>{
 
         obtenerDatos();
      
-    }, [productos.length]);
+    }, []);
 
     useEffect(()=>{
 
@@ -36,7 +42,7 @@ const Productos = () => {
             usuario_id: usuarioConectado.usuario_id
         }
     
-        axios.post("https://backend-lobelbuy.onrender.com/obtenerConversaciones", datos)
+        axios.post("http://localhost:5000/obtenerConversaciones", datos)
         .then(res => {
 
             if(res.data != "No hay conversaciones"){
@@ -53,19 +59,43 @@ const Productos = () => {
 
     const obtenerDatos = () => {
         const datos =  {
-            usuario: usuarioConectado.usuario_id
+            usuario_id: usuarioConectado.usuario_id
         }
 
-        axios.post("https://backend-lobelbuy.onrender.com/mostrarProductos", datos)
+        axios.post("http://localhost:5000/mostrarProductosEnVenta", datos)
         .then(res => {
 
+            setProductosEnVenta(res.data);
             setProductos(res.data);
+            setMensajeMostrar("No tienes productos en venta");
             
         })
         .catch(({response}) => {
             console.log(response.data);
         })
+
+        axios.post("http://localhost:5000/mostrarProductosVendidos", datos)
+        .then(res => {
+
+            setProductosVendidos(res.data);
+            
+        })
+        .catch(({response}) => {
+            console.log(response.data);
+        })
+
+        
     }
+
+    const mostrar = (productos, mensaje) =>{
+        
+        if(mensaje == "en venta"){
+            setMensajeMostrar("No tienes productos en venta");
+        } else {
+            setMensajeMostrar("No has vendido ningún producto");
+        }
+        setProductos(productos);
+    }   
 
     /*
     const mostrarModal = () => {
@@ -80,7 +110,7 @@ const Productos = () => {
 
         console.log(event.target.parentElement.parentElement.firstChild.value)
 
-        axios.post("https://backend-lobelbuy.onrender.com/eliminarProducto", datos)
+        axios.post("http://localhost:5000/eliminarProducto", datos)
         .then(res => {
             if(res.data == "Eliminado") {
                 window.location.reload(true);
@@ -109,7 +139,7 @@ const Productos = () => {
             input.value = 0;
         }
 
-        axios.post("https://backend-lobelbuy.onrender.com/reservarProducto", datos)
+        axios.post("http://localhost:5000/reservarProducto", datos)
         .then(res => {
             console.log(res.data);
         })
@@ -130,7 +160,7 @@ const Productos = () => {
             vendedor_id: usuarioConectado.usuario_id
         }
        
-        axios.post("https://backend-lobelbuy.onrender.com/venderProducto", datos)
+        axios.post("http://localhost:5000/venderProducto", datos)
         .then(res => {
             console.log(res.data)
             
@@ -138,6 +168,8 @@ const Productos = () => {
             if(res.data == "Eliminado") {
                 window.location.reload(true);
             }*/
+            obtenerDatos();
+            //var closeModal = document.getElementById("close");
         })
         .catch(res => {
             console.log(res);
@@ -148,68 +180,79 @@ const Productos = () => {
     return(
         <div id="productosCuenta" className="productosCuenta position-relative" style={{minHeight: "80%",background: "linear-gradient(to bottom, #1E90FF,#87CEEB)"}}>
                 <Sidebar componente="productos"/>
-                <div className="container-fluid py-5">
+                <div className="container-fluid py-5 d-flex flex-column justify-content-center align-items-center">
                     <h1 className="text-white text-center">Mis productos</h1>
-                    <div className="misProductosCuenta d-flex flex-column justify-content-center align-items-center mt-5">
-                        {productos.map((producto) =>{
+                    <div className="row mx-auto pt-5 d-flex justify-content-center">
+                            <div onClick={() => mostrar(productosEnVenta, "en venta")}  className="cajaCategoria col-sm-6 col-xs-6 mb-4 mx-2"><img src={imgSale} style={{width: "60px", height: "60px"}}/><span>En venta</span></div>
+                            <div onClick={() => mostrar(productosVendidos, "vendido")}  className="cajaCategoria col-sm-6 col-xs-6 mb-4 mx-2"><FaRegHandshake style={{width: "60px", height: "60px"}}/><span>Vendido</span></div>
+                    </div>
+                    <div className="w-75 misProductosCuenta d-flex flex-column justify-content-center align-items-center mt-5">
+                        {productos.length == 0 ? (
+                            <h1 className="text-white">{mensajeMostrar}</h1>
+                            ) : (
+                                productos.map((producto) =>{
 
-                            return (
-                                <div className="card horizontal-card mb-3" key={producto.id} style={{width: "50%",background: "linear-gradient(to bottom, #e6f2ff, #99ccff)"}}>
-                                    <div className="row">
-                                        
-                                        <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12 img-container img-overlay position-relative">
-                                            <Link to={"/producto/" + producto.id} className="text-decoration-none">
-                                                {producto.imagen == "" ? (
-                                                    <img src={imgProducto} className="card-img  w-100" height="240" alt="Producto"/>
-                                                ) : (
-                                                    <div>
-                                                        <img src={producto.imagen} className="card-img  w-100" height="240" alt="Producto"/>
-                                                        {producto.reservado == 0 &&
+                                    return (
+                                        <div className="card horizontal-card mb-3" key={producto.id} style={{width: "50%",background: "linear-gradient(to bottom, #e6f2ff, #99ccff)"}}>
+                                            <div className="row">
+                                                
+                                                <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12 img-container img-overlay position-relative">
+                                                    <Link to={"/producto/" + producto.id} className="text-decoration-none">
+                                                        {producto.imagen == "" ? (
+                                                            <img src={imgProducto} className="card-img  w-100" height="240" alt="Producto"/>
+                                                        ) : (
                                                             <div>
-                                                            <span id="reservado" className="fs-4 bg-white rounded-pill p-1 position-absolute" style={{top: "20px", left: "20px"}} hidden>Reservado</span>
-                                                            <input type="number" defaultValue={producto.reservado} hidden/>
+                                                                <img src={producto.imagen} className="card-img  w-100" height="240" alt="Producto"/>
+                                                                {producto.reservado == 0 &&
+                                                                    <div>
+                                                                    <span id="reservado" className="fs-4 bg-white rounded-pill p-1 position-absolute" style={{top: "20px", left: "20px"}} hidden>Reservado</span>
+                                                                    <input type="number" defaultValue={producto.reservado} hidden/>
+                                                                    </div>
+        
+                                                                }
+        
+                                                                {producto.reservado == 1 &&
+                                                                    <div>
+                                                                    <span id="reservado" className="fs-4 bg-white rounded-pill p-1 position-absolute" style={{top: "20px", left: "20px"}}>Reservado</span>
+                                                                    <input type="number" defaultValue={producto.reservado} hidden/>
+                                                                    </div>
+                                                                }   
                                                             </div>
-
-                                                        }
-
-                                                        {producto.reservado == 1 &&
-                                                            <div>
-                                                            <span id="reservado" className="fs-4 bg-white rounded-pill p-1 position-absolute" style={{top: "20px", left: "20px"}}>Reservado</span>
-                                                            <input type="number" defaultValue={producto.reservado} hidden/>
+                                                        )
+        
+                                                        }  
+                                                    </Link> 
+                                                </div>
+                                                
+                                                <div id="caja" className="col-lg-8 col-md-12 col-sm-12 col-xs-12 d-flex justify-content-center align-items-center">
+                                                    <div className="card-body">
+                                                        <div className="row d-flex flex-row">
+                                                            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center d-flex flex-column align-items-center justify-content-center">
+                                                                <input type="number" defaultValue={producto.id} hidden/>
+                                                                <h5 className="card-title">{producto.nombre}</h5>
+                                                                <p className="card-text"><span className="fs-5">Precio: {producto.precio}€</span></p>
                                                             </div>
-                                                        }   
-                                                    </div>
-                                                )
-
-                                                }  
-                                            </Link> 
-                                        </div>
-                                        
-                                        <div id="caja" className="col-lg-8 col-md-12 col-sm-12 col-xs-12 d-flex justify-content-center align-items-center">
-                                            <div className="card-body">
-                                                <div className="row d-flex flex-row">
-                                                    <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center d-flex flex-column align-items-center justify-content-center">
-                                                        <input type="number" defaultValue={producto.id} hidden/>
-                                                        <h5 className="card-title">{producto.nombre}</h5>
-                                                        <p className="card-text"><span className="fs-5">Precio: {producto.precio}€</span></p>
-                                                    </div>
-                                                    <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 mb-2 d-flex flex-column align-items-center justify-content-center">
-                                                        <span className="fs-5">Publicado:</span>
-                                                        <span className="fs-5 fw-bold">20-05-22</span>
-                                                    </div>
-                                                    <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 d-flex flex-column align-items-center justify-content-center">
-                                                        <button className="btn btn-primary" onClick={() => productoElegidoVender(producto.id)} data-bs-toggle="modal" data-bs-target="#exampleModal">Vendido</button>    
-                                                        <button className="btn btn-primary" onClick={(event) => reservarProducto(producto.id, event)}>Reservar</button>                                      
-                                                        <button className="btn btn-danger  my-2" onClick={eliminarProducto}>Eliminar</button>
-                                                        <button className="btn btn-success"><LInk to={"/editarProducto/" + producto.id} className="text-white text-decoration-none">Editar</Link></button>
+                                                            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 mb-2 d-flex flex-column align-items-center justify-content-center">
+                                                                <span className="fs-5">Publicado:</span>
+                                                                <span className="fs-5 fw-bold">20-05-22</span>
+                                                            </div>
+                                                            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 d-flex flex-column align-items-center justify-content-center">
+                                                                <button className="btn btn-primary" onClick={() => productoElegidoVender(producto.id)} data-bs-toggle="modal" data-bs-target="#exampleModal">Vendido</button>    
+                                                                <button className="btn btn-primary" onClick={(event) => reservarProducto(producto.id, event)}>Reservar</button>                                      
+                                                                <button className="btn btn-danger  my-2" onClick={eliminarProducto}>Eliminar</button>
+                                                                <button className="btn btn-success"><Link to={"/editarProducto/" + producto.id} className="text-white text-decoration-none">Editar</Link></button>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
+                                    )
+                                })
                             )
-                        })}
+
+                        }
+                        
                     </div>
                 </div>
                 <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -217,7 +260,7 @@ const Productos = () => {
                         <div className="modal-content">
                             <div className="modal-header">
                                 <h5 className="modal-title" id="exampleModalLabel">Producto Vendido</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" id="close" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="modal-body">
                                 <h6>¿A quien se lo has vendido?</h6>
@@ -227,13 +270,13 @@ const Productos = () => {
                                         <div key={conversaciones.id}>
                                             {conversaciones.usuario1_id == usuarioConectado.usuario_id &&
                                                 
-                                                <span className="name mt-2" style={{cursor: "pointer"}} onClick={() => venderProducto(conversaciones.usuario2_id)}>{conversaciones.nombre_usuario2}</span>
+                                                <span className="name mt-2" style={{cursor: "pointer"}} onClick={() => venderProducto(conversaciones.usuario2_id)} data-bs-dismiss="modal">{conversaciones.nombre_usuario2}</span>
                                                     
                                             }
             
                                             {conversaciones.usuario2_id == usuarioConectado.usuario_id &&
                                             
-                                                <span className="name" style={{cursor: "pointer"}} onClick={() => venderProducto(conversaciones.usuario1_id)}>{conversaciones.nombre_usuario1}</span>
+                                                <span className="name" style={{cursor: "pointer"}} onClick={() => venderProducto(conversaciones.usuario1_id)} data-bs-dismiss="modal">{conversaciones.nombre_usuario1}</span>
                                                     
                                             }
                                         </div>
@@ -241,7 +284,7 @@ const Productos = () => {
                                     })
                                 
                                 }
-                                <span className="text-danger" style={{cursor: "pointer"}} onClick={() => venderProducto(null)}>Lo he vendido en otro lado</span>
+                                <span className="text-danger" style={{cursor: "pointer"}} onClick={() => venderProducto(null)} data-bs-dismiss="modal">Lo he vendido en otro lado</span>
                             </div>
                         </div>
                     </div>
