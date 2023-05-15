ae9e8759eb62bde793d3cc34d5c0345ddc655a87
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-16 00:13:21 +0200

    Update Favoritos.js

--- src/componentes/Favoritos.js
+++ src/componentes/Favoritos.js
@@ -27,7 +27,7 @@ const Favoritos = () => {
             usuario_id: usuarioConectado.usuario_id
         }
 
-        axios.post("https://backend-lobelbuy.onrender.com/mostrarProductosFavoritos", datos)
+        axios.post("http://localhost:5000/mostrarProductosFavoritos", datos)
         .then(res => {
 
             setProductos(res.data);
@@ -43,39 +43,46 @@ const Favoritos = () => {
     return(
         <div id="favoritos" className="position-relative" style={{minHeight: "80%", background: "linear-gradient(to bottom, #1E90FF,#87CEEB)"}}>
             <Sidebar componente="favoritos"/>
-            <div className="container-fluid py-5">
+            <div className="container-fluid py-5 d-flex flex-column justify-content-center align-items-center">
                     <h1 className="text-white text-center">Mis favoritos</h1>
-                    <div className="misProductosCuenta d-flex flex-column justify-content-center align-items-center mt-5">
+                    <div className="w-75 misProductosCuenta d-flex flex-column justify-content-center align-items-center mt-5">
                         {loader && <Loader/>}
-                        {productos.map((producto) =>{
+                        {productos.length == 0 ? (
+                            <h3 className="text-white">No tienes ningún producto en favoritos</h3>
+                        ) : (
+                            productos.map((producto) =>{
 
-                            return (
-                                <div class="card horizontal-card mb-3" key={producto.id} style={{width: "50%",background: "linear-gradient(to bottom, #e6f2ff, #99ccff)"}}>
-                                    <div class="row no-gutters">
-                                        <div class="col-md-4 img-container img-overlay">
-                                            {producto.imagen == "" ? (
-                                                <img src={imgProducto} class="card-img  w-100" height="270" alt="Producto"/>
-                                            ) : (
-                                                <img src={producto.imagen} class="card-img  w-100" height="270" alt="Producto"/>
-                                            )
-
-                                            }
-                                            
-                                        </div>
-                                        <div class="col-md-8">
-                                            <div class="card-body d-flex flex-column align-items-start justify-content-start">
-                                                <input type="number" value={producto.id} hidden/>
-                                                <h5 class="card-title">{producto.nombre}</h5>
-                                                <p class="card-text"><span class="fs-5">Precio: {producto.precio}€</span></p>
-                                                <div className="d-flex flex-column">
-                                                    <button className="btn btn-primary"><Link to={"/producto/" + producto.id} className="text-white text-decoration-none">Ver producto</Link></button>
+                                return (
+                                    <div class="card horizontal-card mb-3" key={producto.id} style={{width: "50%",background: "linear-gradient(to bottom, #e6f2ff, #99ccff)"}}>
+                                        <div class="row no-gutters">
+                                            <div class="col-md-4 img-container img-overlay">
+                                                {producto.imagen == "" ? (
+                                                    <img src={imgProducto} class="card-img  w-100" height="270" alt="Producto"/>
+                                                ) : (
+                                                    <img src={producto.imagen} class="card-img  w-100" height="270" alt="Producto"/>
+                                                )
+    
+                                                }
+                                                
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body d-flex flex-column align-items-start justify-content-start">
+                                                    <input type="number" value={producto.id} hidden/>
+                                                    <h5 class="card-title">{producto.nombre}</h5>
+                                                    <p class="card-text"><span class="fs-5">Precio: {producto.precio}€</span></p>
+                                                    <div className="d-flex flex-column">
+                                                        <button className="btn btn-primary"><Link to={"/producto/" + producto.id} className="text-white text-decoration-none">Ver producto</Link></button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })}
+                                )
+                            })
+                        )
+                           
+                        }
+                        
                     </div>
                 </div>
         </div>
