8b51bc9d59a74f23aa638755ebe9ef2b99d904e8
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-18 17:31:23 +0200

    Update ProductoInicio.js

--- src/componentes/ProductoInicio.js
+++ src/componentes/ProductoInicio.js
@@ -2,15 +2,13 @@ import React, { useEffect } from "react"
 import {Link} from "react-router-dom"
 import imgProducto from "./iconos/crash.jpg"
 import "./styles/productoInicio.css"
-import meGustaOff from "./iconos/me-gusta.png"
-import meGustaOn from "./iconos/me-gusta-on.png"
 
 const ProductoInicio = (props) => {
 
     var tamanoCSS = "";
 
     if(props.tamano == 1){
-        tamanoCSS = "col-lg-4 col-md-8 col-sm-12 col-xs-12";
+        tamanoCSS = "col-lg-9 col-md-8 col-sm-12 col-xs-12";
     } else if(props.tamano == 2) {
         tamanoCSS = "col-lg-4 col-md-8 col-sm-6 col-xs-12";
     } else if(props.tamano == 3) {
@@ -23,11 +21,11 @@ const ProductoInicio = (props) => {
     
     return(
              
-            <div id="productoInicio" class={tamanoCSS + " mb-5"}>
-                <div class="card text-center" style={{borderRadius: "20px 20px 20px 20px", overflow: "hidden"}} >
+            <div id="productoInicio" class={"mx-3 mb-3"}>
+                <div class="card text-center" style={{borderRadius: "20px 20px 20px 20px", overflow: "hidden", width: "350px", boxShadow: "4px 4px 4px black"}} >
                     <div>
                         <Link to={"/producto/" + props.id}>
-                            <div class="position-relative" data-mdb-ripple-color="light" style={{height: "350px"}}>
+                            <div class="position-relative" data-mdb-ripple-color="light" style={{height: "250px"}}>
                                 <img id="imgPro" src={props.imagen} class="w-100 h-100" style={{borderRadius: "20px 20px 0 0"}} />
                                 {props.reservado == 1 &&
                                     <span className="position-absolute" style={{left: 0, bottom: 0}}>
@@ -44,10 +42,10 @@ const ProductoInicio = (props) => {
                             </div>
                         </Link>
                     </div>
-                    <div class="card-body position-relative" style={{borderRadius: "0 0 20px 20px", background: "linear-gradient(to bottom, #e6f2ff, #99ccff)"}}>
-                        <h5 class="card-title mb-3 fw-bold">{props.nombre}</h5> 
-                        <p>{props.categoria}</p>
-                        <h6 class="mb-3">{props.precio}€</h6>
+                    <div class="card-body position-relative" style={{borderRadius: "0 0 20px 20px", backgroundColor: "aliceblue"}}>
+                        <h5 class="card-title  fw-bold">{props.nombre}</h5> 
+                        <span>{props.categoria}</span>
+                        <h6 class="">{props.precio}€</h6>
                     </div>
                 </div>
             </div>
