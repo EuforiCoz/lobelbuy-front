fd6f1fbaa254656f4fe54faa17878599553341b5
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-19 01:40:05 +0200

    Update ProductoInicio.js

--- src/componentes/ProductoInicio.js
+++ src/componentes/ProductoInicio.js
@@ -2,6 +2,10 @@ import React, { useEffect } from "react"
 import {Link} from "react-router-dom"
 import imgProducto from "./iconos/crash.jpg"
 import "./styles/productoInicio.css"
+import {FaRegHandshake} from "react-icons/fa"
+import {TiCancelOutline} from "react-icons/ti"
+import {BsFillPencilFill} from "react-icons/bs"
+import {IoMdSave} from "react-icons/io"
 
 const ProductoInicio = (props) => {
 
@@ -18,6 +22,33 @@ const ProductoInicio = (props) => {
     } else {
         tamanoCSS = "col-lg-4 col-md-6 col-sm-6 col-xs-12";
     }
+/*
+    const ponerColor = (boton,index) => {
+        console.log(index)
+        var caja = document.getElementsByClassName(boton)[index];
+        var imagen = caja.firstChild;
+        if(boton == "vender") {
+            caja.style.borderColor = "#1E90FF";
+            imagen.style.fill = "#1E90FF";
+        }  else if(boton == "reservar"){
+            caja.style.borderColor = "purple";
+            imagen.style.fill = "purple";
+        }else if(boton == "editar"){
+            caja.style.borderColor = "rgb(0, 154, 0)";
+            imagen.style.fill = "rgb(0, 154, 0)";
+        } else {
+            caja.style.borderColor = "red";
+            imagen.style.fill = "red";
+        }
+    }
+
+    const quitarColor = (boton,index) => {
+        console.log(index)
+        var caja = document.getElementsByClassName(boton)[index];
+        var imagen = caja.firstChild;
+        caja.style.borderColor = "black";
+        imagen.style.fill = "black";
+    }*/
     
     return(
              
@@ -46,6 +77,14 @@ const ProductoInicio = (props) => {
                         <h5 class="card-title  fw-bold">{props.nombre}</h5> 
                         <span>{props.categoria}</span>
                         <h6 class="">{props.precio}€</h6>
+                        {/*props.componente == "cuenta" &&
+                            <div className="d-flex flex-row justify-content-center align-items-center">
+                                <div onMouseOver={() => ponerColor("vender", props.index)} onMouseOut={() => quitarColor("vender", props.index)} className="vender venderDiv rounded-circle d-flex justify-content-center align-items-center mx-3" style={{width: "45px", height: "45px"}}><FaRegHandshake className="venderImg" style={{width: "75%", height:"75%"}}/></div>
+                                <div onMouseOver={() => ponerColor("reservar", props.index)} onMouseOut={() => quitarColor("reservar", props.index)} className="reservar venderDiv rounded-circle d-flex justify-content-center align-items-center mx-3" style={{width: "45px", height: "45px"}}><IoMdSave className="venderImg" style={{width: "75%", height:"75%"}}/></div>
+                                <div onMouseOver={() => ponerColor("editar", props.index)} onMouseOut={() => quitarColor("editar", props.index)} className="editar venderDiv rounded-circle d-flex justify-content-center align-items-center mx-3" style={{width: "45px", height: "45px"}}><BsFillPencilFill className="venderImg" style={{width: "55%", height:"55%"}}/></div>
+                                <div onMouseOver={() => ponerColor("eliminar", props.index)} onMouseOut={() => quitarColor("eliminar", props.index)} className="eliminar venderDiv rounded-circle d-flex justify-content-center align-items-center mx-3" style={{width: "45px", height: "45px"}}><TiCancelOutline className="venderImg" style={{width: "80%", height:"80%"}}/></div>
+                            </div>*/
+                        }
                     </div>
                 </div>
             </div>
