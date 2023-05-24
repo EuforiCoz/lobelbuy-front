f154adbd92f5eb813fec63e3efc7bcb7ed9204d5
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-24 21:05:33 +0200

    Update Header.js

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -60,56 +60,54 @@ const Header = () => {
     
     return(
         <div id="header" className="container-fluid py-4">
-            <div className="row">
-                <div className="col-md-2 col-xs-4  text-white d-flex justify-content-center align-items-center">
-                    <Link to="/" className="text-decoration-none">
-                        <div className="logo text-white">Lobelbuy</div>
-                    </Link>
-                </div>
-                <div id="buscador" className="buscador col-md-7 col-xs-8 text-white d-flex flex-row-reverse">
-                    {/*<input id="nombreBuscador" type="text" placeholder="Buscar producto" className="inputBuscar form-control w-100 rounded-pill"/>*/}
-                    <input id="nombreBuscador" type="text" placeholder="Buscar producto" className="inputBuscar form-control w-100"/>
-                    <img id="busca" src={iconoBuscar} className="iconoBuscar me-2" alt="buscar" onClick={buscarProducto}/>
-                </div>
-                <div className="col-md-1 text-white d-flex justify-content-center align-items-center cuenta">
-                {usuario != null ?
-                        (
-                        <div className="botonCuenta">
-                            <div className="d-flex flex-row" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                               
-                                <img src={usuarioConectado.imagen} className="rounded-circle iconoUsuario mx-2" alt="Foto de perfil" width="40" height="40"/>
-                                <div className="cuenta text-white fs-5">{usuario.nombre}</div>
-                            </div>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><Link className="dropdown-item" to="/cuenta/productos">Mi cuenta</Link></li>
-                                <li className="dropdown-item" onClick={cerrarSesion}>Cerrar Sesión</li>
-                            </ul>
-                        </div>
-                        ) : (
-                            <div className="botonCuenta">
-                            <div className="d-flex flex-row" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <div className="cuenta text-white">Cuenta</div>
-                                <img src={iconoUsuario} className="iconoUsuario ms-1" alt="cuenta"/>
-                            </div>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                                <li><Link className="dropdown-item" to="/login">Iniciar sesión</Link></li>
-                                <li><Link className="dropdown-item" to="/registro">Registrarse</Link></li>
-                            </ul>
-                        </div>)
-                }
-                    
-                </div>
-                {/*
-                <div className="col-1 text-white">
-                    <div className="botonMensajes d-flex flex-row">
-                        <div className="mensajes fw-bold">MENSAJES</div>
-                        <img src={iconoMensajes} className="iconoChat ms-2" alt="chat"/>
+            <div className="row d-flex justify-content-center align-items-center" >
+                <div className="col-lg-3 col-md-3 col-sm-6 col-6">
+                    <div className="text-white d-flex justify-content-center align-items-center">
+                        <Link to="/" className="text-decoration-none">
+                            <div className="logo text-white">Lobelbuy</div>
+                        </Link>
                     </div>
                 </div>
-                */}
-                <div  className="col-md-2 subirProducto">
-                    {/*<Link to="/subirProducto" className="inputSubir btn text-white border fw-bold rounded-pill">Subir Producto</Link>*/}
-                    <Link to="/subirProducto" className="inputSubir btn text-white">Subir Producto</Link>
+                <div className="col-lg-5 col-md-5 col-sm-6 col-6">
+                    <div id="buscador" className="buscador text-white d-flex flex-row-reverse align-items-center">
+                        {/*<input id="nombreBuscador" type="text" placeholder="Buscar producto" className="inputBuscar form-control w-100 rounded-pill"/>*/}
+                        <input id="nombreBuscador" type="text" placeholder="Buscar producto" className="inputBuscar form-control w-100"/>
+                        <img id="busca" src={iconoBuscar} className="iconoBuscar me-2" alt="buscar" onClick={buscarProducto}/>
+                    </div>
+                </div>
+                <div className="col-lg-2 col-md-2 col-sm-6 col-6">
+                    <div className="text-white d-flex justify-content-center align-items-center cuenta">
+                    {usuario != null ?
+                            (
+                            <div className="botonCuenta">
+                                <div className="d-flex flex-row" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src={usuarioConectado.imagen} className="rounded-circle iconoUsuario mx-2" alt="Foto de perfil" width="40" height="40"/>
+                                    <div className="cuenta nombre-usuario text-white fs-5" >{usuario.nombre}</div>
+                                </div>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li><Link className="dropdown-item" to="/cuenta/productos">Mi cuenta</Link></li>
+                                    <li className="dropdown-item" onClick={cerrarSesion}>Cerrar Sesión</li>
+                                </ul>
+                            </div>
+                            ) : (
+                                <div className="botonCuenta">
+                                <div className="d-flex flex-row" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <div className="cuenta text-white">Cuenta</div>
+                                    <img src={iconoUsuario} className="iconoUsuario ms-1" alt="cuenta"/>
+                                </div>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                                    <li><Link className="dropdown-item" to="/login">Iniciar sesión</Link></li>
+                                    <li><Link className="dropdown-item" to="/registro">Registrarse</Link></li>
+                                </ul>
+                            </div>)
+                    }
+                    </div>
+                </div>
+                <div className="col-lg-2 col-md-2 col-sm-6 col-6">
+                    <div  className="subirProducto">
+                        {/*<Link to="/subirProducto" className="inputSubir btn text-white border fw-bold rounded-pill">Subir Producto</Link>*/}
+                        <Link to="/subirProducto" className="inputSubir btn text-white">Subir Producto</Link>
+                    </div>
                 </div>
             </div> 
            
