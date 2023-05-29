b022a8927bd9ceea3b473f9a524fa29e34c17f46
Eufori98 <125218275+Eufori98@users.noreply.github.com>
2023-05-29 07:52:51 +0200

    Update ResenaForm.js

--- src/componentes/ResenaForm.js
+++ src/componentes/ResenaForm.js
@@ -43,7 +43,7 @@ const Cuenta = () => {
             seterrorValoracion(true);
         }else{
             document.getElementById("modalEnviandoResena").style.display = "block";
-            axios.post("http://localhost:5000/enviarResena", datos)
+            axios.post("https://backend-lobelbuy.onrender.com/enviarResena", datos)
             .then(res => {
                 document.getElementById("modalEnviandoResena").style.display = "none";
                 navigate("/cuenta/resenas");
